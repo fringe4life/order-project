a98376767a623246f11de2da84eb154fd72c7542
Coinnich <>
2025-01-11 17:54:11 +1300

    added some responsiveness

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import lightningcss from "vite-plugin-lightningcss"
-
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
-      
       deno(),
       lightningcss({
         browserslist: 'last 3 versions',
         minify: true
-      }),
-      
+      })     
   ],
   css: {
     transformer: 'lightningcss'
   },
   build: {
-    cssMinify: 'lightningcss'
+    cssMinify: 'lightningcss',
+    terserOptions: {
+      format: {
+        comments: false
+      },
+    },
   },
   esbuild: {
     drop: ['console', 'debugger']
